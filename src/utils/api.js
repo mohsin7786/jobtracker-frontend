2c5bf7b39e9ccf4f61eb67d90023f4e9b8c727ef
mohsin7786 <mohsin7786@gmail.com>
2026-05-12 10:15:31 +0530

    update API base URL for production

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  // baseURL: 'http://localhost:5000/api'
+  baseURL: 'https://jobtracker-backend-n5ht.onrender.com/api'
 })
 
 // Har request mein token automatically add ho
