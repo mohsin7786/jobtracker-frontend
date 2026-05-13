661335b0763bcbc73f5eea4b208c4037b97a210b
mohsin7786 <mohsin7786@gmail.com>
2026-05-13 16:00:46 +0530

    auto login

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import ProtectedRoute from './components/ProtectedRoute'
 function App() {
+  const token = localStorage.getItem('token')
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Navigate to={token ? "/dashboard" : "/login"} />} />
+        <Route path="/login" element={token ? <Navigate to='/dashboard' /> : <Login />} />
+        <Route path="/register" element={token ? <Navigate to='/dashboard' /> : <Register />} />
         <Route path="/dashboard" element={
           <ProtectedRoute>
             <Dashboard />
