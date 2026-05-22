c5ff53588f5050b0c16f02ceaa4f2c4909289391
mohsin7786 <mohsin7786@gmail.com>
2026-05-22 17:51:16 +0530

    fix delete confirm modal and server wake up

--- src/utils/api.js
+++ src/utils/api.js
@@ -14,6 +14,15 @@ API.interceptors.request.use((config) => {
   return config
 })
 
+const wakeUpServer = async () => {
+  try {
+    await API.get('/')
+  } catch (err) {
+    console.log('Server waking up...')
+  }
+}
+wakeUpServer()
+
 export const registerUser = (data) => API.post('/auth/register', data)
 export const loginUser = (data) => API.post('/auth/login', data)
 export const getJobs = () => API.get('/jobs')
